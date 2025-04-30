3342c33da385f89a1b51b830f3f87df1f0cdb173
Harikaran <harikaran7242@gmail.com>
2025-04-30 13:41:08 +0530

    Refactor App.js and Navbar components; add Certifications section and update constants. Improved code formatting and added new skills and experiences.

---
+++ src/components/Certifications/index.js
@@ -0,0 +1,84 @@
+import React from "react";
+import styled from "styled-components";
+import { certifications } from "../../data/constants";
+import CertificationCard from "../Cards/CertificationCard";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: relative;
+  z-index: 1;
+  align-items: center;
+  padding: 0px 0px 60px 0px;
+  @media (max-width: 960px) {
+    padding: 0px;
+  }
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  max-width: 1350px;
+  padding: 40px 0px 0px 0px;
+  gap: 12px;
+  @media (max-width: 960px) {
+    flex-direction: column;
+  }
+`;
+
+const Title = styled.div`
+  font-size: 42px;
+  text-align: center;
+  font-weight: 600;
+  margin-top: 20px;
+  color: ${({ theme }) => theme.text_primary};
+  @media (max-width: 768px) {
+    margin-top: 12px;
+    font-size: 32px;
+  }
+`;
+
+const Desc = styled.div`
+  font-size: 18px;
+  text-align: center;
+  max-width: 600px;
+  color: ${({ theme }) => theme.text_secondary};
+  @media (max-width: 768px) {
+    margin-top: 12px;
+    font-size: 16px;
+  }
+`;
+
+const TimelineSection = styled.div`
+  width: 100%;
+  max-width: 1000px;
+  margin-top: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 12px;
+`;
+
+const Certifications = () => {
+  return (
+    <Container id="certifications">
+      <Wrapper>
+        <Title>Certifications</Title>
+        <Desc>Here are some of my certifications and achievements.</Desc>
+        <TimelineSection>
+          {certifications.map((certification, index) => (
+            <CertificationCard key={index} certification={certification} />
+          ))}
+        </TimelineSection>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Certifications;
